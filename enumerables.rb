874e139129606e4774a0c3dc30faf38515998388
Taiwo Coker <51359447+taiwocoker@users.noreply.github.com>
2020-05-14 09:31:35 +0100

    add my_each, my_select, my_each_with_index,my_all methods

--- enumerables.rb
+++ enumerables.rb
@@ -2,7 +2,7 @@
 # rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 module Enumerable
   def my_each
-    return unless block_given?
+    return to_enum(:my_each) unless block_given?
 
     for i in self
       yield i
@@ -10,7 +10,7 @@ module Enumerable
   end
 
   def my_each_with_index
-    return unless block_given?
+    return to_enum(:my_each_with_index) unless block_given?
 
     index = 0
     for i in self
@@ -20,11 +20,39 @@ module Enumerable
   end
 
 
+  def my_select
+    return to_enum(:my_select) unless block_given?
 
+    if is_a?(Array)
+      arr = []
+      my_each do |num|
+        arr << num if yield(num)
+      end
+      arr
+    elsif is_a?(Hash)
+      hash = {}
+      my_each do |key, val|
+        hash[key] = val if yield(key, val)
+      end
+      hash
+    end
+  end
 
+  def my_all?
+    return to_enum(:my_all) unless block_given?
 
+    result = my_select { |x| yield x }
+
+    length == result.length
+  end
 
 end
+
+arr=[1,2,3,4,5,6]
+
+p arr.my_each
+p arr.each
+
 
 # rubocop: enable Metrics/ModuleLength
 # rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
