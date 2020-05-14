4f1282caf16ea2167fdb301c1973fd84ef206bd7
Taiwo Coker <51359447+taiwocoker@users.noreply.github.com>
2020-05-14 13:51:27 +0100

    add inject method

--- enumerables.rb
+++ enumerables.rb
@@ -75,6 +75,29 @@ module Enumerable
     array
   end
 
+  def my_inject(accumulator = nil, operation = nil, &block)
+    block = case operation
+      when Symbol
+        lambda { |acc, value| acc.send(operation, value) }
+      when nil
+        block
+      else
+      raise ArgumentError, "the operation provided must be a symbol"
+    end
+    if accumulator.nil?
+      ignore_first = true
+      accumulator = first
+    end
+    index = 0
+    each do |element|
+      unless ignore_first && index == 0
+        accumulator = block.call(accumulator, element)
+      end
+      index += 1
+    end
+    accumulator
+  end
+
 
 end
 
